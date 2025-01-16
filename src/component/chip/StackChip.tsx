c30978fb30b7320e4629912d580ac11faadb553c
IM-HYUN-JEONG <fltxld3@naver.com>
2025-01-16 21:57:42 +0900

    fix: 스택칩 아이콘 src 없을경우 추가

--- src/component/chip/StackChip.tsx
+++ src/component/chip/StackChip.tsx
@@ -1,5 +1,6 @@
 import { Tooltip } from '@nextui-org/react';
 import Image from 'next/image';
+import { useState } from 'react';
 
 interface StackChipProp {
   title?: string;
@@ -8,6 +9,8 @@ interface StackChipProp {
 }
 
 export const StackChip = ({ title, stackList, size = 25 }: StackChipProp) => {
+  const [isImageVisible, setImageVisible] = useState(true);
+
   return (
     <div className="mt-2">
       {title && <span className="text-sm"># {title}</span>}
@@ -15,13 +18,15 @@ export const StackChip = ({ title, stackList, size = 25 }: StackChipProp) => {
         {stackList.map((stack, index) => (
           <Tooltip content={stack || 'Unknown Stack'} key={index}>
             <div className="flex flex-row items-center gap-1">
-              <Image
-                src={`/icons/${stack.toLowerCase()}.svg`}
-                onError={(e) => (e.currentTarget.src = '/icons/default.svg')} // 기본 이미지 대체
-                height={size}
-                width={size}
-                alt={stack || 'Unknown Stack'}
-              />
+              {isImageVisible && (
+                <Image
+                  src={`/icons/${stack?.toLowerCase() || 'default'}.svg`} // stack이 없으면 default로 설정
+                  onError={(e) => (e.currentTarget.src = '/icons/default.svg')} // 이미지 로드 실패 시 대체
+                  height={size}
+                  width={size}
+                  alt={stack || 'Unknown Stack'}
+                />
+              )}
             </div>
           </Tooltip>
         ))}
